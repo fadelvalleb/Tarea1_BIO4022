cbf989d570092aca7f176be62e408c72633ee40f
fadelvalleb <54915646+fadelvalleb@users.noreply.github.com>
2019-09-15 19:44:23 -0300

    Se modificó la base de datos para eliminar a cetacea porque presentaba una distribución muy lejos del resto

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Tarea1_BIO4022
+# Tarea 1 - BIO4022
 ## Acá se incluirá el contenido de la tarea 1. Esto es una primera prueba del uso de GIT para información reproducible.
--- Script de prueba para DB.R
+++ Script de prueba para DB.R
@@ -1,11 +1,20 @@
-### filtrado de datos para obtener columnas por orden y comparar su distribución
+### se cargan las librerías necesarias para trabajar en el entorno
+
+library(tidyverse)
+library(ggplot2)
+
+### A continuación se hace un filtrado de datos para obtener columnas por
+### orden y comparar su distribución
 ### espacial. Se importó la base de datos con el header modificado desde el .txt
 ### Luego se ejecuta el siguiente script para obtener Ordenes y distribución por Km2
 ### Se hizo además un filter por los valores mayores a 0 descartando aquellos negativos.
-### esto hace que las obser
+
+
 
 Homerange <- Pantheria_DB %>% select(MSW05_Order, X22.1_HomeRange_km2) %>% 
-  group_by(MSW05_Order) %>% filter(X22.1_HomeRange_km2 > 0) %>% 
+  group_by(MSW05_Order) %>% filter (MSW05_Order != "Cetacea") %>% 
+  filter(X22.1_HomeRange_km2 > 0) %>% 
+
   rename(Orden=MSW05_Order, Distribución=X22.1_HomeRange_km2)
 
 ### Se cambiará el nombre de las columnas para que sean más lógicas con respecto
@@ -19,5 +28,4 @@ Mean_Homerange<-summarise(Homerange,  Distribución = mean(Distribución))
 
 ### acá se probará el tipo de gráfico a utilizar con las funciones de ggplot
 ### 
-
 
