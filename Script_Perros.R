734ad33b005bdde6af3fea419476e323796bfcda
fadelvalleb <54915646+fadelvalleb@users.noreply.github.com>
2019-09-17 16:55:06 -0300

    Se incorporó el comando para importar el archivo de Pantheria_DB solucionando el error

--- Script_Perros.R
+++ Script_Perros.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(ggplot2)
 
+Pantheria_DB <- read.delim("~/Tarea1_BIO4022/Pantheria_DB.txt")
+View(Pantheria_DB)
 
 Perros <- Pantheria_DB %>% select(MSW05_Genus, X5.1_AdultBodyMass_g, MSW05_Binomial) %>%
   group_by(MSW05_Genus) %>% filter(MSW05_Genus == "Canis") %>% 
